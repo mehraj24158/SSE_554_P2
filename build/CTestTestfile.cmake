f09ea5dbf367f059cd1f3283faf756f2dc96deb9
Mehraj_Ahmed <mehraj24158@gmail.com>
2021-02-15 17:27:14 -0500

    cleared build folder

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Mehraj_Ahmed/Workspace/SSE_554/P2/SSE_554_P2
-# Build directory: C:/Users/Mehraj_Ahmed/Workspace/SSE_554/P2/SSE_554_P2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
-subdirs("test")
--- build/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Mehraj_Ahmed/Workspace/SSE_554/P2/SSE_554_P2/googletest/googlemock
-# Build directory: C:/Users/Mehraj_Ahmed/Workspace/SSE_554/P2/SSE_554_P2/build/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
