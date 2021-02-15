0826c78faa11d44e43e6f394a9b0f1312e6a2106
Mehraj_Ahmed <mehraj24158@gmail.com>
2021-02-15 17:20:45 -0500

    added build folder and settings

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Mehraj_Ahmed/Workspace/SSE_554/P2/SSE_554_P2
+# Build directory: C:/Users/Mehraj_Ahmed/Workspace/SSE_554/P2/SSE_554_P2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googletest")
+subdirs("test")
---
+++ build/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Mehraj_Ahmed/Workspace/SSE_554/P2/SSE_554_P2/googletest
+# Build directory: C:/Users/Mehraj_Ahmed/Workspace/SSE_554/P2/SSE_554_P2/build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
