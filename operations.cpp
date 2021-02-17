90ca59578a89b2bc303b0dd425aea87198f636f6
Mehraj_Ahmed <mehraj24158@gmail.com>
2021-02-16 21:03:13 -0500

    added operations

--- operations.cpp
+++ operations.cpp
@@ -4,11 +4,18 @@ void EngineCreator();
 void BodyCreator();
 void TireCreator();
 
+// Create Factory object containing threads. 
+// Pass car object to the factory object and let it finish the car
+// Create more complex interplay between subthreads.
+// Essentially, You cant install an engine without having a car body ready. 
 void main()
 {
-    std::thread t2(BodyCreator);
-    std::thread t1(EngineCreator);
-    std::thread t2(TireCreator)
+    std::thread t1(BodyCreator);
+    t1.join();
+    std::thread t2(EngineCreator);
+    t2.join();
+    std::thread t3(TireCreator);
+    t3.join();
 }
 
 void EngineCreator()
