d33c9ee5074fe241f31923019e43b566fa65ebbd
Usman Khan <khan.usmanr@gmail.com>
2021-02-20 16:59:18 -0500

    Updated Engine class

--- engine.hpp
+++ engine.hpp
@@ -1,11 +1,18 @@
 #pragma once
-
+#include <iostream>
+using namespace std; 
 class Engine{ 
 public: 
     // attributes
-    
+    int enginesize; 
+    string status;  
+
     // constructors
     Engine();
 
     // methods
+    int engineSize(); 
+    string engineStatus(); 
+
+
 };
